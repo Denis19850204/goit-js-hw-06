86c8be53ab4ca392297ecf078f6412135ce6578b
Denis19850204 <dg04021985@gmail.com>
2022-08-17 08:04:12 +0300

    first

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = document.createElement('li');
+itemEl.classList.add('item')
+console.log(itemEl)
+
+for (const ingredient of ingredients) {
+  console.log(ingredient)
+  itemEl.textContent = ingredient;
+}
